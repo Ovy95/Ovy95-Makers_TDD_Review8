f0738a1fabfc1cd431d8fb6d95bbc901159ed819
Ovy95 <jackoverton21@hotmail.com>
2021-04-23 09:36:27 +0100

    default soundwave set up compelete

--- spec/bandpassSpec.js
+++ spec/bandpassSpec.js
@@ -16,8 +16,12 @@ describe("BandPass", function () {
       expect(bandPass.filter([1020])).toEqual([1000])
     })
 
-    it("give a [1020] return the max limit (1000) default",function() {
+    it("given [30,20] return [40,40]",function() {
       expect(bandPass.filter([30,20])).toEqual([40,40])
+    })
+
+    it("given [1003,20,57] returns [1000,40,57]",function() {
+      expect(bandPass.filter([1003,20,57])).toEqual([1000,40,57])
     })
 
   })

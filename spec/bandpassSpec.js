c121afab6f4891ddae2871dae7b7ea297ab1825e
Ovy95 <jackoverton21@hotmail.com>
2021-04-23 09:27:06 +0100

    passing test for lower limit

--- spec/bandpassSpec.js
+++ spec/bandpassSpec.js
@@ -6,8 +6,11 @@ describe("BandPass", function () {
   })
 
   describe("Filter Method",function () {
-    it("Returns array,value we give first hardcoded test",function() {
+    it("Returns single array,value",function() {
       expect(bandPass.filter([40])).toEqual([40])
+    })
+    it("give a [0] return the minimum limit (40) default",function() {
+      expect(bandPass.filter([39])).toEqual([40])
     })
 
   })
--- src/bandpass.js
+++ src/bandpass.js
@@ -2,6 +2,9 @@
 class BandPass {
 
   filter(soundWaveArray){
+    if(soundWaveArray[0] < 40) {
+      soundWaveArray[0] = 40 
+    }
     return soundWaveArray
 
   }
